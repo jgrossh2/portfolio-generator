c8b771b9116fa56417eaf85f2cd25f788089b2c9
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-26 10:06:42 -0500

    inquirer installed

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio Complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio Complete! Check out index.html to see the output!')
+// });
